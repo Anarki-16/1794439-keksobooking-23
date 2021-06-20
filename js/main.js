2537f9cfc6bc171656e159c6c4b5bc4c053dc756
Aleksei <anarki166@gmail.com>
2021-06-20 18:06:09 +0200

    Домашнее задание второго раздела попытка N100

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const getRandomFloat=(min, max, places = 0) => {
   const value = (Math.random() * (max - min + 1)) + min;
   return Number.parseFloat( value.toFixed (places));
@@ -7,14 +6,3 @@ const getRandomFloat=(min, max, places = 0) => {
 const getRandomInteger = (min, max) => getRandomFloat (min, max );
 
 // источник: https://www.udacity.com/blog/2021/04/javascript-random-numbers.html
-=======
-const getRandomFloat = (min, max, places = 0) => {
-    let value = (Math.random() * (max - min + 1)) + min;
-    return Number.parseFloat(value.toFixed(places));
-};
-
-const getRandomInteger = (min, max) => {
-    return getRandomFloat(min, max);
-}
-//источник: https://www.udacity.com/blog/2021/04/javascript-random-numbers.html
->>>>>>> 699e11e40b48f0d419b901070a8647e077e925a7
